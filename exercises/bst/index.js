050bae36d83b70ebad9dd6b00d8ac55872685d99
berkansivri <berkansivri@gmail.com>
2020-12-12 22:04:05 +0300

    bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,58 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+
+  // recursive
+  contains(data) {
+    if (this.data === data) {
+      return this;
+    }
+
+    if (data < this.data && this.left) {
+      return this.left.contains(data);
+    } else if (data > this.data && this.right) {
+      return this.right.contains(data);
+    }
+    return null;
+  }
+
+  // // loop
+  // contains(data) {
+  //   let node = this;
+  //   while (node) {
+  //     if (data < node.data) {
+  //       node = node.left;
+  //     } else if (data > node.data) {
+  //       node = node.right;
+  //     } else {
+  //       return node;
+  //     }
+  //   }
+  //   return null;
+  // }
+}
+
+const node = new Node(10);
+node.insert(5);
+node.insert(15);
+node.insert(17);
 
 module.exports = Node;
--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -4,7 +4,37 @@
 // less than the parent node's value, and that
 // every node's right hand child is greater than
 // the parent
+const Node = require("./node");
 
-function validate(node, min = null, max = null) {}
+function validate(
+  node,
+  min = Number.MIN_SAFE_INTEGER,
+  max = Number.MAX_SAFE_INTEGER
+) {
+  if (!node) return true;
+  if (node.data >= max || node.data <= min) return false;
+  return (
+    validate(node.left, min, node.data) && validate(node.right, node.data, max)
+  );
+}
 
 module.exports = validate;
+
+// const n = new Node(1);
+// n.left = new Node(1);
+// console.log(validate(n));
+
+// const n = new Node(10);
+// n.insert(5);
+// n.insert(15);
+// n.insert(0);
+// n.insert(20);
+// n.left.left.right = new Node(999);
+// console.log(validate(n));
+
+// const n = new Node(10);
+// n.insert(5);
+// n.insert(15);
+// n.insert(0);
+// n.insert(20);
+// console.log(validate(n));
