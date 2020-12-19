16db098c94258125c925d368341cd51cebfe2bbe
berkansivri <berkansivri@gmail.com>
2020-12-19 16:21:30 +0300

    merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,10 +1,8 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function merge(left, right) {}
-
 const bubbleSort = require("./bubbleSort");
 const selectionSort = require("./selectionSort");
-const mergeSort = require("./mergeSort");
+const { merge, mergeSort } = require("./mergeSort");
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
--- exercises/sorting/mergeSort.js
+++ exercises/sorting/mergeSort.js
@@ -1,3 +1,28 @@
-function mergeSort(arr) {}
+function merge(left, right) {
+  const merged = [];
 
-module.exports = mergeSort;
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      merged.push(left.shift());
+    } else {
+      merged.push(right.shift());
+    }
+  }
+
+  return [...merged, ...left, ...right];
+}
+
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+
+  const mid = arr.length / 2;
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+const res = mergeSort([97, 0, 22, -30]);
+console.log(res);
+
+module.exports = { mergeSort, merge };
